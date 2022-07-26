b5c697dd54bc9702e8901dd9f2499aec95ba6f1d
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-07-26 18:41:31 +0300

    Добавление Main.java и README.md

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] sales = {20, 0, 190, 25, 191};
+        SalesManager salesManager = new SalesManager(sales);
+
+        System.out.println("Максимальный размер продаж: " + salesManager.max());
+    }
+}
+
