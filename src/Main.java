4bd701e2f2c46cc8f1ac2b3dec7d9d2dcaaa0c20
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-07-31 21:53:24 +0300

    Поменяли int на long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = {20, 0, 190, 25, 191};
+        long[] sales = {20, 0, 190, 25, 191};
         SalesManager salesManager = new SalesManager(sales);
 
         System.out.println("Максимальный размер продаж: " + salesManager.max());
